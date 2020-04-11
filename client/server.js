c846e604853fd69d515c3109c6b2d66bc33306fa
zodix420 <davarashly@gmail.com>
2020-04-11 22:04:12 +0300

    yo

--- client/server.js
+++ client/server.js
@@ -1,17 +1,35 @@
-const http = require('http');
-const port = 3000;
+const http = require("http")
+const fs = require("fs").promises
+const port = 3000
 
 const requestHandler = (request, response) => {
-    console.log(request.url);
-    response.end('Hello, Node.js server!');
+    console.log(request.url)
+
+    if (request.url === "/") {
+        fs.readFile("./html/ajax-start.html", "utf-8").then(file => {
+            return response.end(file)
+        })
+    } else if (request.url.match(/^\/verses\/\d{1,}$/g)) {
+        const verseId = request.url.split("/")[2]
+
+        fs.readFile(`./verses/verse${verseId}.txt`, "utf-8")
+          .then(file => {
+              return response.end(file)
+          })
+          .catch(err => {
+              return response.end("No such file with id " + verseId)
+          })
+    } else {
+        return response.end("404")
+    }
 }
 
-const server = http.createServer(requestHandler);
+const server = http.createServer(requestHandler)
 
-server.listener(port, err) => {
+server.listen(port, err => {
     if (err) {
-        return console.log('something bad happened', err);
+        return console.log("something bad happened", err)
     }
 
-    console.log('server is listening on ${port}')
-}
+    console.log(`Server is listening on ${port}`)
+})
