11c12ca5c5e7dc0de96f3988e7f2dca0dc635e07
zodix420 <davarashly@gmail.com>
2020-04-11 22:18:00 +0300

    yo

--- client/server.js
+++ client/server.js
@@ -1,35 +1,39 @@
-const http = require("http")
-const fs = require("fs").promises
+const http = require('http')
+const fs = require('fs').promises
 const port = 3000
 
 const requestHandler = (request, response) => {
-    console.log(request.url)
+  console.log(request.url)
 
-    if (request.url === "/") {
-        fs.readFile("./html/ajax-start.html", "utf-8").then(file => {
-            return response.end(file)
-        })
-    } else if (request.url.match(/^\/verses\/\d{1,}$/g)) {
-        const verseId = request.url.split("/")[2]
+  if (request.url === '/') {
+    fs.readFile('./html/ajax-start.html', 'utf-8').then((file) => {
+      return response.end(file)
+    })
+  } else if (request.url.includes('style.css')) {
+    fs.readFile('./html/style.css', 'utf-8').then((file) => {
+      return response.end(file)
+    })
+  } else if (request.url.match(/^\/verses\/\d{1,}$/g)) {
+    const verseId = request.url.split('/')[2]
 
-        fs.readFile(`./verses/verse${verseId}.txt`, "utf-8")
-          .then(file => {
-              return response.end(file)
-          })
-          .catch(err => {
-              return response.end("No such file with id " + verseId)
-          })
-    } else {
-        return response.end("404")
-    }
+    fs.readFile(`./verses/verse${verseId}.txt`, 'utf-8')
+      .then((file) => {
+        return response.end(file)
+      })
+      .catch((err) => {
+        return response.end('No such file with id ' + verseId)
+      })
+  } else {
+    return response.end('404')
+  }
 }
 
 const server = http.createServer(requestHandler)
 
-server.listen(port, err => {
-    if (err) {
-        return console.log("something bad happened", err)
-    }
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
 
-    console.log(`Server is listening on ${port}`)
+  console.log(`Server is listening on ${port}`)
 })
