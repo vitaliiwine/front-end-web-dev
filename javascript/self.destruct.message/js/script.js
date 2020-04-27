6b91dd008d71be3bdfe664768e8fe39490fb891b
vitaliiwine <wineeewineee@gmail.com>
2020-04-27 09:52:13 +0300

    javascript jedi

---
+++ javascript/self.destruct.message/js/script.js
@@ -0,0 +1,7 @@
+alert("Warning! This message will self-destruct in");
+alert("3...");
+alert("2...");
+alert("1...");
+
+document.querySelector("h1").textContent = "🔥BOOM!🔥";
+console.log("Message destroyed!");
