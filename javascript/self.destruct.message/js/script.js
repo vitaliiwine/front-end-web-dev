6468b6d59809e90fe25f1dc9941dbc4ff3252ec6
vitaliiwine <wineeewineee@gmail.com>
2020-04-28 14:26:46 +0300

    minor changes

--- javascript/self.destruct.message/js/script.js
+++ javascript/self.destruct.message/js/script.js
@@ -5,3 +5,10 @@ alert("1...");
 
 document.querySelector("h1").textContent = "🔥BOOM!🔥";
 console.log("Message destroyed!");
+
+const  htmlSnippet = '<h1 class="headline">My headline</h1>';
+const message = 'I \'m a programmer!';
+
+const multiline = "Hello everyone.\
+    I'm writing JavaScript code \
+    Working with string elements";
