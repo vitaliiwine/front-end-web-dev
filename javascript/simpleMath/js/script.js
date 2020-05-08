db3d40ba3f7c4e4d246614421d14c0fd013bc97e
vitaliiwine <wineeewineee@gmail.com>
2020-05-08 17:22:26 +0300

    new structure, shortened

--- javascript/simpleMath/js/script.js
+++ javascript/simpleMath/js/script.js
@@ -12,27 +12,29 @@ let message;
 
 num0 = prompt("Please type a number.");
 num0 = parseFloat(num0);
-if (isNaN(num0)) {
-    alert("Seems like it is not a number you typed. Redo and refresh.")
-}
-
 num1 = prompt("Now enter the second one.");
 num1 = parseFloat(num1);
+
+// Preventing an input to be NaN or zero
+
 if (num1 === 0) {
-    alert("You're going to divide by zero. No way you can do this. Redo and refresh.")
+    alert("You're going to divide by zero. No way you can do this. Refresh and try again.")
+} else if (isNaN(num0) || isNaN(num1)) {
+    alert("Seems like one of the inputs is not a number. Refresh and try again.")
+} else {
+    message = `<h1>Math with the numbers ${num0} and ${num1}</h1>`; // building a string
+    message += `${num0} + ${num1} = ${num0 + num1}`;
+    message += `<br>`;
+    message += `${num0} - ${num1} = ${num0 - num1}`;
+    message += `<br>`;
+    message += `${num0} * ${num1} = ${num0 * num1}`;
+    message += `<br>`;
+    message += `${num0} / ${num1} = ${num0 / num1}`;
+
+    // Output
+
+    document.querySelector('main').innerHTML = message;
 }
 
-// Building a message
 
-message = `<h1>Math with the numbers ${num0} and ${num1}</h1>`;
-message += `${num0} + ${num1} = ${num0 + num1}`;
-message += `<br>`;
-message += `${num0} - ${num1} = ${num0 - num1}`;
-message += `<br>`;
-message += `${num0} * ${num1} = ${num0 * num1}`;
-message += `<br>`;
-message += `${num0} / ${num1} = ${num0 / num1}`;
-
-// Output
-
-document.querySelector('main').innerHTML = message;
+1
