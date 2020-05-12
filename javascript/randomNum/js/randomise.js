75f582f6cd383699c13ef12f973f682f5424cea2
vitaliiwine <wineeewineee@gmail.com>
2020-05-12 17:14:34 +0300

    final

--- javascript/randomNum/js/randomise.js
+++ javascript/randomNum/js/randomise.js
@@ -1,14 +1,22 @@
+/*The program accepts two numbers and gives back one
+* in an interval between two given numbers */
+
+// Storing input
+
 const lowIn = prompt("Give me a low number");
 const higIn = prompt("Give me a high one");
+
+// Converting strings into numbers
 
 const lowNum = parseInt(lowIn);
 const highNum = parseInt(higIn);
 
-function getRandomArbitrary() {
-    return Math.random() * (highNum - lowNum) + lowNum;
-}
+// Logic, output
 
-document.querySelector('main').innerHTML = `The random number between
-${lowNum} and ${highNum} is ${getRandomArbitrary()}`;
+if (highNum && lowNum) {
+    const randomNum = Math.floor(Math.random() * (highNum - lowNum + 1)) + lowNum;
+    document.querySelector('main').innerHTML = `The random number between
+${lowNum} and ${highNum} is ${randomNum}.`;
+} else alert("Sorry, that won't work. Provide numbers.");
 
 
