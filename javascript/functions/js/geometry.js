24083a70a98bab5efd281b3f0e8a0be10ac4a8cc
vitaliiwine <wineeewineee@gmail.com>
2020-05-14 17:33:41 +0300

    final

--- javascript/functions/js/geometry.js
+++ javascript/functions/js/geometry.js
@@ -19,13 +19,14 @@ function areaCircle(radius) {
 // The function calculates the volume of a sphere
 
 function volumeSphere(radius) {
-    return (4/3) * Math.PI * Math.pow(radius, 3);
+    return (4 / 3) * Math.PI * Math.pow(radius, 3);
 }
 
 // Templates for output each function
 
-console.log(`The area of a rectangle is ${areaRectangle()}
-            The volume of a rectangular prism is ${volumeRecPrism()}
-            The area of a circle is ${areaCircle()}
-            The volume of a sphere is ${volumeSphere()}
+console.log(`
+The area of a rectangle is ${areaRectangle(5, 22)}
+The volume of a rectangular prism is ${volumeRecPrism(4.5, 12.5, 17.4)}
+The area of a circle is ${areaCircle(7.2)}
+The volume of a sphere is ${volumeSphere(7.2)}
 `);
