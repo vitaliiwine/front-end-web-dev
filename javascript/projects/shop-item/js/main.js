186bb2e8c7d7c003445bce969037af0a9050fcaf
vitaliiwine <wineeewineee@gmail.com>
2020-07-22 20:28:06 +0300

    refresh

---
+++ javascript/projects/shop-item/js/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Spinner',
+        image: './img/item.jpg',
+        description: "Led-flash spinner",
+        price: "$4.99",
+        inventory: 7,
+        onSale: true,
+        cart: 0
+    }
+});
