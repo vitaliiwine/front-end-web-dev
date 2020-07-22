e8f6f792772c0474f2ac8d8b0c7e0fb7df1696bb
vitaliiwine <wineeewineee@gmail.com>
2020-07-22 17:25:16 +0300

    refresh

---
+++ javascript/projects/the-page/js/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Spinner',
+        image: './img/blue.jpg',
+        description: "A bundle of steel colourful spinners",
+        inventory: 100,
+        onSale: true,
+        details: ["66% steel", "34% plastic", "Speed Adjustments"],
+        cart: 0
+    }
+});
--- javascript/the_page/js/main.js
+++
@@ -1,8 +0,0 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        product: 'Spinner',
-        image: './img/product_pic.jpg',
-        description: "A bundle of steel colourful spinners"
-    }
-});
