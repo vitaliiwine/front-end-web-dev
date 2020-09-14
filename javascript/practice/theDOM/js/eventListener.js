76ee0e7fc30d31c530ec3a6e7e32ce39d8aeee19
vitaliiwine <wineeewineee@gmail.com>
2020-09-14 09:59:27 +0300

    adding list items

--- javascript/practice/theDOM/js/eventListener.js
+++ javascript/practice/theDOM/js/eventListener.js
@@ -1,10 +1,64 @@
+//Change heading color
+
 // Selecting an HTML element
 
 const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
+const headingButton = document.getElementById('headingButton');
+const headingColor = document.getElementById('headingColor');
 
 // Adding event listener
 
-myButton.addEventListener('click', () => {
-    myHeading.style.color = 'tomato';
+headingButton.addEventListener('click', () => {
+    myHeading.style.color = headingColor.value;
 });
+
+// Hide Cart List
+
+const toggleButton = document.getElementById('toggleButton');
+const listDiv = document.querySelector('.list');
+console.log('what?');
+
+toggleButton.addEventListener('click', () => {
+    if (listDiv.style.display == 'none') {
+        toggleButton.textContent = 'Hide List Items';
+        listDiv.style.display = 'block';
+    } else {
+        toggleButton.textContent = 'Show List Items';
+        listDiv.style.display = 'none';
+    }
+});
+
+// Add List Items
+
+const addItemInput = document.getElementById('addItemInput');
+const addItemButton = document.getElementById('addItemButton');
+
+addItemButton.addEventListener('click', () => {
+    let li = document.createElement('li');
+    li.textContent = addItemInput.value;
+});
+
+//Change list items color
+
+const myList = document.getElementsByTagName('li');
+const colorListButton = document.getElementById('colorListButton');
+const listColor = document.getElementById('listColor');
+
+colorListButton.addEventListener('click', () => {
+    for (let i = 0; i < myList.length; i++) {
+        myList[i].style.color = listColor.value
+    }
+});
+
+//Change list items heading
+
+const listHeading = document.querySelector('h4');
+const listHeadingOption = document.querySelector('input#listHeadingOption');
+const listHeadingButton = document.querySelector('button#listHeadingButton');
+
+listHeadingButton.addEventListener('click', () => {
+    console.log('hey');
+    listHeading.textContent = listHeadingOption.value + `:`;
+});
+
+
